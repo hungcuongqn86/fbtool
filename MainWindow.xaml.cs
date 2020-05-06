0cfa00e6f4c6b00272108becddc9dee3a11566de
cuongnh <hungcuongqn86@gmail.com>
2020-05-06 23:26:11 +0700

    initial commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium.Chrome;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace fbtool
 {
@@ -21,7 +23,7 @@ namespace fbtool
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+        ChromeDriver chromeDriver;
         public MainWindow()
         {
             InitializeComponent();
@@ -29,18 +31,30 @@ namespace fbtool
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            chromeDriver.Quit();
         }
 
         private void Button_Next_Click(object sender, RoutedEventArgs e)
         {
+            KillChrome();
             ChromeOptions options = new ChromeOptions();
             options.AddArgument("--user-data-dir=C:\\Users\\hungc\\AppData\\Local\\Google\\Chrome\\User Data");
+            options.AddArgument("profile-directory=Profile 1");
             options.AddArgument("disable-infobars");
+            options.AddArgument("--disable-extensions");
             options.AddArgument("--start-maximized");
-            ChromeDriver chromeDriver = new ChromeDriver(options);
+            chromeDriver = new ChromeDriver(options);
             chromeDriver.Url = "https://www.facebook.com/";
             chromeDriver.Navigate();
+        }
+
+        private void KillChrome()
+        {
+            Process[] processNames = Process.GetProcessesByName("chrome");
+            foreach (Process item in processNames)
+            {
+                item.Kill();
+            }
         }
     }
 }
